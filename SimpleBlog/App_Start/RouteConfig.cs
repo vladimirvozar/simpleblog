e685a2bbd336ae787c4e1f0c9555c4057c44fed4
Vladimir Vozar <vvozar@yahoo.com>
2019-10-14 09:34:01 +0200

    Implemented form authentication and custom role provider

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace SimpleBlog
 
             routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
 
+            routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
+
             routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
         }
     }
