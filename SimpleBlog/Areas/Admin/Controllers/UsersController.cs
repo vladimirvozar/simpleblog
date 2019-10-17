35575a3ecaeb0297000867ced8418e1f8e9a8443
Vladimir Vozar <vvozar@yahoo.com>
2019-10-17 16:25:05 +0200

    Prevented CSRF attacks

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -26,7 +26,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             return View(new UsersNew { });
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult New(UsersNew form)
         {
             if (Database.Session.Query<User>().Any(u => u.Username == form.Username))
@@ -60,7 +60,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             });
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Edit(int id, UsersEdit form)
         {
             var user = Database.Session.Load<User>(id);
@@ -92,7 +92,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             });
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult ResetPassword(int id, UsersResetPassword form)
         {
             var user = Database.Session.Load<User>(id);
@@ -110,7 +110,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             return RedirectToAction("index");
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
             var user = Database.Session.Load<User>(id);
