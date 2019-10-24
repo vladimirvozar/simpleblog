0782d5f4d19bba74113e14cbf2f0777147702672
Vladimir Vozar <vvozar@yahoo.com>
2019-10-24 15:01:58 +0200

    Worked on frontend Posts controller; implemented all markup for views

---
+++ SimpleBlog/ViewModels/Posts.cs
@@ -0,0 +1,25 @@
+﻿using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsShow
+    {
+        public Post Post { get; set; }
+    }
+
+    public class PostsTag
+    {
+        public Tag Tag { get; set; }
+        public PagedData<Post> Posts { get; set; }
+    }
+}
